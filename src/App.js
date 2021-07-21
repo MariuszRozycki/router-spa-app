0706dffce53af1dcb797aeec47f4b903fb7bafd2
MariuszRozycki <mariansc83@icloud.com>
2021-07-21 12:24:43 +0200

    Add initial router-spa-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+const Home = () => <h1>Main website</h1>
+const News = () => <h1>News</h1>
+const Contact = () => <h1>Contact</h1>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li><Link to="/">Start</Link></li>
+                <li><Link to="/news">News</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <section>
+            <Route path="/" exact component={Home} />
+            <Route path="/news" component={News} />
+            <Route path="/contact" component={Contact} />
+          </section>
+        </div>
+      </Router>
     );
   }
 }
